03df13d271ad61870e6b3ebc3d927653d8ac853b
henryln1 <henritienne@gmail.com>
2018-05-24 10:35:26 -0700

    wrote more processing code for movie corpus

--- data_processing.py
+++ data_processing.py
@@ -5,9 +5,9 @@ from collections import defaultdict
 import os
 
 
-cornell_movie_delimiter = '+++$+++' 
+cornell_movie_delimiter = ' +++$+++ ' 
 
-directory_location = '../cornell-movie-dialogs-corpus/'
+#directory_location = '../cornell-movie-dialogs-corpus/'
 
 title_metadata_txt_file = 'movie_titles_metadata.txt'
 
@@ -19,6 +19,10 @@ movie_lines_txt_file = 'movie_lines.txt'
 
 all_txt_files = [title_metadata_txt_file, characters_metadata_txt_file, conversations_txt_file, movie_lines_txt_file]
 
+all_genres = ['drama', 'horror', 'action', 'comedy', 'crime', 'sci-fi', 'adventure', 'fantasy', 
+				'animation', 'romance', 'music', 'war', 'thriller', 'mystery', 'biography', 'western', 
+				'musical']
+
 
 def read_txt_file(file_name):
 	'''
@@ -28,41 +32,82 @@ def read_txt_file(file_name):
 
 	useful_information_list = [] #list of lists
 
-	with open(file_name) as f:
-		lines = f.readLines()
+	with open(file_name, encoding = 'latin-1') as f:
+		#lines = f.readLines()
 
-		for line in lines:
+		for line in f:
 			#print(line)
+			#print(line)
+			#line.replace(" ", "")
 			split_line = line.split(cornell_movie_delimiter)
 			#print(split_line)
+			#split_line = [x.replace(" ", "") for x in split_line]
+			split_line[-1] = split_line[-1][:-1]
 			useful_information_list.append(split_line)
 
 	return useful_information_list
 
 
-def read_text_file_diff_directory(path_name):
+# def read_text_file_diff_directory(path_name):
 
-	#useful_information_list
-	filehandle = open(path_name)
-	print(filehandle.read())
-	filehandle.close()
+# 	#useful_information_list
+# 	filehandle = open(path_name)
+# 	print(filehandle.read())
+# 	filehandle.close()
 
-def read_movie_text_files(directory_location, text_files):
+def read_movie_text_files(text_files):
 
 	text_file_dict = defaultdict(list)
-	fileDir = os.path.dirname(os.path.realpath('__file__'))
+	#fileDir = os.path.dirname(os.path.realpath('__file__'))
 
 	for text_file in text_files:
 
-		path = os.path.join(fileDir, directory_location + text_file)
-		path = os.path.abspath(os.path.realpath(path))
-		print(path)
-		text_file_dict[text_file] = read_text_file_diff_directory(path)
+		#path = os.path.join(fileDir, directory_location + text_file)
+		#path = os.path.abspath(os.path.realpath(path))
+		#print(path)
+		text_file_dict[text_file] = read_txt_file(text_file)
 
 	return text_file_dict
 
-read_movie_text_files(directory_location, all_txt_files)
+# temp_dict = read_movie_text_files(all_txt_files)
 
+# print(temp_dict.keys())
+
+# for key in temp_dict:
+# 	print(temp_dict[key][0])
+
+def extract_genres(genre_string):
+	list_of_genres = []
+	for genre in all_genres:
+		if genre in genre_string:
+			list_of_genres.append(genre)
+
+	return list_of_genres
+
+def dict_form_title_metadata(info_list):
+	#info list is a list of lists like ['m0', '10 things i hate about you', '1999', '6.90', '62847', "['comedy', 'romance']"]
+	#returns a dict from movie index number and name to important info
+
+	'''
+	returned dictionary has form dict[(movie_index, movie_title)][year/rating/imdb_votes/genres]
+
+
+	'''
+	all_movies_metadata_dict = {}
+	for entry in info_list:
+		curr_movie_dict = {
+			'year' : entry[2],
+			'rating' : entry[3],
+			'imdb_votes' : entry[4],
+			'genres' : extract_genres(entry[5])
+		}
+		all_movies_metadata_dict[(entry[0], entry[1])] = curr_movie_dict
+
+	return all_movies_metadata_dict
+
+
+def convert_lists_to_dictionaries(text_file_dict):
+	movie_metadata_dict = dict_form_title_metadata(text_file_dict[title_metadata_txt_file])
 
 
 
