9d8f5ef27adf3a7a6cf45db54fb8f2cf23e79444
Kaylee Bement <kbement@stanford.edu>
2018-06-04 13:09:20 -0700

    counts and correct file path

--- blog_processing.py
+++ blog_processing.py
@@ -5,21 +5,25 @@ import random
 # returns dict {topic -> {gender -> post}}
 def get_post_data():
     posts = {}
-    csv_df = pd.read_csv('blogtext.csv', encoding = 'latin-1')
+    csv_df = pd.read_csv('../blogtext.csv', encoding = 'latin-1')
     csv_array = csv_df.as_matrix()
+    counter = 0
     for row in csv_array:
         topic = row[3].strip()
         if topic not in posts:
             posts[topic] = {'m': [], 'f': []}
         gender = row[1][0]
         post = row[6].strip()
-        posts[topic][gender].append(post) 
+        posts[topic][gender].append(post)
+        counter += 1 
+    print(counter)
     return posts
 
 # returns list of lines and associated genders w equal # lines from each topic
 def get_labeled_lines_equal(unprocessed):
     posts = []
     genders = []
+    counter = 0
     for topic, data in unprocessed.items():
         male_posts = data['m']
         female_posts = data['f']
@@ -32,8 +36,12 @@ def get_labeled_lines_equal(unprocessed):
         genders += ['m' for i in range(num_convos)]
         posts += female_equal
         genders += ['f' for i in range(num_convos)]
+        counter += num_convos * 2
+    print(counter)
     return posts, genders
 
 def get_blog_data():
     posts_unprocessed = get_post_data()
     return get_labeled_lines_equal(posts_unprocessed)
+
+get_labeled_lines_equal(get_post_data())
